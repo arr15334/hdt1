0d12f359bf653e5a986629ab07c22a95db30db96
arriaza96 <arriaza96@172.20.14.196>
2016-07-06 11:28:51 -0600

    Primera version

---
+++ radiohdt1/src/radio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo Arriaza;
+ */
+public interface radio {
+    public double subirEstacion();  
+    public double bajarEstacion();
+    public void encender();
+    public void apagar();
+}
+
